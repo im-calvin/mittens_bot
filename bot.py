464dae0293548632f973e6f87e353d663794cd4e
calv <kelvinwong0519@gmail.com>
2022-05-08 18:12:55 -0700

    heroku hosting

--- bot.py
+++ bot.py
@@ -20,6 +20,11 @@ translate_client = translate.Client.from_service_account_json(
 
 
 @client.event
+async def on_ready():
+    print("もしもし")
+
+
+@client.event
 async def on_message(message):
     if message.author == client.user:  # base case
         return
@@ -27,29 +32,37 @@ async def on_message(message):
     if translate_client.detect_language(message.content)["language"] != "en" and translate_client.detect_language(message.content)["confidence"] > 0.95:
         transl_msg = translate_client.translate(message.content, "en", "text")[
             "translatedText"]  # transl_msg = translated form of message
-        # if the message contains a ':' and is less than 12 characters (to catch stray emotes)
-        # if message.content.find(':') != -1 and len(message.content) < 12:
-        #     return
 
         # so that the bot does not re-output links
         if message.content.startswith("https://"):
             return
 
         # making the emotes format themselves properly!
-        if "<:" in message.content and ":>" in message.content:
+        if "<:" in message.content and ">" in transl_msg:
+            # "indices_open" is the list that contains the index's for the '<' char
             indices_open = [i.start() for i in re.finditer('<', transl_msg)]
             indices_closed = [i.start() for i in re.finditer('>', transl_msg)]
-            # index1 = transl_msg.find('<')  # location of the first '<'
-            # index2 = transl_msg.find('>')
 
             # if there's an emote there should be equal number of < and >
-            for i in indices_open:
-                emote = transl_msg[indices_open:indices_closed] # contains the str for the emote
-            # remove all the " " inside the emote (hopefully this makes it output properly)
-                emote = emote.replace(" ", "")
+            for i in range(len(indices_open)):
+                # splicing each emote from transl_msg into a list of str
+                emote = [transl_msg[indices_open[i]:indices_closed[i]]]
+            # remove all the " " inside the emote
+                emote[i] = emote[i].replace(" ", "")
+                # text after first emote and before second emote
+                j = 0
+                while j < len(indices_open)-1:
+                    main_txt = [
+                        transl_msg[indices_closed[j]:indices_open[j+1]]]
+                    j += 1
+                # all the text between the first and last emote
+                body_txt = emote[i] + main_txt[i]
             # all the text that does not contain the emotes
-                main_txt = transl_msg[0:indices_open]
-            await message.channel.send(main_txt + emote)
+            # text before first emote
+            main_txt_start = transl_msg[0:indices_open[0]]
+            main_txt_end = transl_msg[indices_closed[len(indices_closed)]:len(
+                transl_msg)]  # text after last emote
+            await message.channel.send(main_txt_start + body_txt + main_txt_end)
         else:
             await message.channel.send(transl_msg)
 
@@ -57,12 +70,6 @@ async def on_message(message):
     if message.content == "a":
         await message.channel.send("サメです！")
 
-# i just want the emotes to translate through well
 
 client.run(TOKEN)
 translate.run(TRANSLATE)
-
-# testing...
-# result = re.search('(.*)', message.content)
-# elif client.hash(message.content) != 0: # if the message does not contain an emote
-#     await message.channel.send
