c949e3ac8580dee2b294adacc8324ad3f95cfcf1
calv <kelvinwong0519@gmail.com>
2022-05-09 23:35:24 -0700

    sanitize all emotes-stable

--- bot.py
+++ bot.py
@@ -29,57 +29,32 @@ async def on_message(message):
         return
     if message.author.bot:  # bot doesn't respond to other bots
         return
+    if message.content.startswith("https://"):
+        return
+    if message.content.startswith("<:"):
+        return
+    lang = translate_client.detect_language(message.content)["language"]
 
     # if jap is detected & the confidence is high
-    if translate_client.detect_language(message.content)["language"] == "ja" or translate_client.detect_language(message.content)["language"] == "zh":
+    if lang == "ja" or lang == "zh" or lang == "fr" or lang == "ko":
         if translate_client.detect_language(message.content)["confidence"] > 0.9:
             transl_msg = translate_client.translate(message.content, "en", "text")[
                 "translatedText"]  # transl_msg = translated form of message
 
             if "@" in transl_msg:
                 transl_msg = transl_msg.replace("@ ", "@")
-            if transl_msg.startswith("https://"):
-                return
-            if transl_msg.startswith("<:"):
-                return
 
         # making the emotes format themselves properly!
-
-            if (("<:" in transl_msg and ">" in transl_msg) or ("<a:" in transl_msg)):
-                # check if the msg contains an emoji that it cannot use:
-                # "indices_open" is the list that contains the index's for the '<' char
-                # while True:
-                #     index1 = transl_msg.find('<')
-                #     index2 = transl_msg.find('>')
-                #     transl_msg = transl_msg.replace(
-                #         transl_msg[index1:index2], "")
-                #     if transl_msg.find('<') > -1:
-                #         break
-                indices_open = [i.start() for i in re.finditer('<', transl_msg)]
-                indices_closed = [i.start() for i in re.finditer('>', transl_msg)]
-                # if there's an emote there should be equal number of < and >
-                for i in range(len(transl_msg)):  # number of emotes
-                    # splicing each emote from transl_msg into a list of str
-                    # 'emote_id' is a 'list' of all the 'ids' of the emotes in 'transl_msg'
-                    emote_id = [int(s) for s in re.findall(
-                        r'\b\d+\b', transl_msg[indices_open[i]:indices_closed[i]])]
-                    # 'emote_object' is a 'list' of object type 'emoji' with respective 'emote_id'
-                    print(emote_id[i])
-                    emote_object = [client.get_emoji(emote_id[i])]
-                    if emote_object[i].is_usable() == False:
-                        for i in range(len(indices_open)):
-                            transl_msg = transl_msg.replace(
-                                transl_msg[indices_open[i]:indices_closed[i]], "")
-                        # sanitizer function
-                        while True:
-                            index1 = transl_msg.find('<')
-                            index2 = transl_msg.find('>')
-                            transl_msg = transl_msg.replace(
-                                transl_msg[index1:index2], "")
-                            if transl_msg.find('<') > -1:
-                                break
-                else:
-                    transl_msg = transl_msg.replace(": ", ":")
+            if (("<:" in transl_msg and ">" in transl_msg) or ("<a:" in transl_msg and ">" in transl_msg)):
+                # sanitizer function
+                while True:
+                    # since slicing is exclusive of index1, inclusive of index2
+                    index1 = transl_msg.find('<')
+                    index2 = transl_msg.find('>') + 1
+                    transl_msg = transl_msg.replace(
+                        transl_msg[index1:index2], "")
+                    if transl_msg.find('<') == -1:
+                        break
             await message.channel.send(transl_msg)
 
     # for gura-chan
